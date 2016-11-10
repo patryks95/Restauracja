87c9358bff66eb50969e946eed15469fda54b52f
Lenovo <Lenovo@E540>
2016-11-10 21:59:38 +0100

    Logowanie

--- Restauracja/Form1.Designer.cs
+++ Restauracja/Form1.Designer.cs
@@ -28,34 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Przycisk_Logowania = new System.Windows.Forms.Button();
+            this.Etykieta_Logowanie = new System.Windows.Forms.Label();
+            this.Tekst_Logowanie = new System.Windows.Forms.TextBox();
+            this.Etykieta_Haslo = new System.Windows.Forms.Label();
+            this.Tekst_Haslo = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // Przycisk_Logowania
             // 
-            this.button1.Location = new System.Drawing.Point(243, 177);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(192, 100);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "HeLLo!!!";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Przycisk_Logowania.Location = new System.Drawing.Point(489, 451);
+            this.Przycisk_Logowania.Name = "Przycisk_Logowania";
+            this.Przycisk_Logowania.Size = new System.Drawing.Size(203, 56);
+            this.Przycisk_Logowania.TabIndex = 0;
+            this.Przycisk_Logowania.Text = "Zaloguj się";
+            this.Przycisk_Logowania.UseVisualStyleBackColor = true;
+            this.Przycisk_Logowania.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Etykieta_Logowanie
+            // 
+            this.Etykieta_Logowanie.AutoSize = true;
+            this.Etykieta_Logowanie.Font = new System.Drawing.Font("Arial Unicode MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Etykieta_Logowanie.Location = new System.Drawing.Point(407, 96);
+            this.Etykieta_Logowanie.Name = "Etykieta_Logowanie";
+            this.Etykieta_Logowanie.Size = new System.Drawing.Size(106, 40);
+            this.Etykieta_Logowanie.TabIndex = 1;
+            this.Etykieta_Logowanie.Text = "Login:";
+            // 
+            // Tekst_Logowanie
+            // 
+            this.Tekst_Logowanie.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Tekst_Logowanie.Location = new System.Drawing.Point(414, 188);
+            this.Tekst_Logowanie.Name = "Tekst_Logowanie";
+            this.Tekst_Logowanie.Size = new System.Drawing.Size(349, 34);
+            this.Tekst_Logowanie.TabIndex = 2;
+            // 
+            // Etykieta_Haslo
+            // 
+            this.Etykieta_Haslo.AutoSize = true;
+            this.Etykieta_Haslo.Font = new System.Drawing.Font("Arial Unicode MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Etykieta_Haslo.Location = new System.Drawing.Point(407, 252);
+            this.Etykieta_Haslo.Name = "Etykieta_Haslo";
+            this.Etykieta_Haslo.Size = new System.Drawing.Size(109, 40);
+            this.Etykieta_Haslo.TabIndex = 3;
+            this.Etykieta_Haslo.Text = "Hasło:";
+            // 
+            // Tekst_Haslo
+            // 
+            this.Tekst_Haslo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Tekst_Haslo.Location = new System.Drawing.Point(414, 333);
+            this.Tekst_Haslo.Name = "Tekst_Haslo";
+            this.Tekst_Haslo.Size = new System.Drawing.Size(349, 34);
+            this.Tekst_Haslo.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(744, 502);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.Tekst_Haslo);
+            this.Controls.Add(this.Etykieta_Haslo);
+            this.Controls.Add(this.Tekst_Logowanie);
+            this.Controls.Add(this.Etykieta_Logowanie);
+            this.Controls.Add(this.Przycisk_Logowania);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Przycisk_Logowania;
+        private System.Windows.Forms.Label Etykieta_Logowanie;
+        private System.Windows.Forms.TextBox Tekst_Logowanie;
+        private System.Windows.Forms.Label Etykieta_Haslo;
+        private System.Windows.Forms.MaskedTextBox Tekst_Haslo;
     }
 }
 
--- Restauracja/Form1.cs
+++ Restauracja/Form1.cs
@@ -7,9 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data;
+using System.Data.SqlClient;
 namespace Restauracja
 {
+   
+
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -19,7 +23,36 @@ namespace Restauracja
 
         private void button1_Click(object sender, EventArgs e)
         {
+            dane logowanie = new dane();
+            SqlConnection sqlConn = new SqlConnection("Data Source=192.168.1.101,1433;Network Library=dbmssocn;Initial Catalog = Northwind; User ID = " + Tekst_Logowanie.Text + ";Password = " + Tekst_Haslo.Text + ";");
+            try
+            {
 
+                // otwórz połączenie:
+                sqlConn.Open();
+                logowanie.Ustaw_login(Tekst_Logowanie.ToString());
+                logowanie.Ustaw_haslo(Tekst_Haslo.ToString());
+                Tekst_Logowanie.Dispose();
+                Etykieta_Logowanie.Dispose();
+                Tekst_Haslo.Dispose();
+                Etykieta_Haslo.Dispose();
+                Przycisk_Logowania.Dispose();
+                sqlConn.Close();
+            }
+            catch (System.Data.SqlClient.SqlException se)
+            {
+                MessageBox.Show(se.Message,se.Source);
+
+            }
+
+
+        }
+        class dane
+        {
+            string login = "";
+            string hasło = "";
+            public void Ustaw_login(string l) { login = l; }
+            public void Ustaw_haslo(string h) { hasło = h; }
         }
     }
 }
